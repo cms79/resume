446b4ea6581e6c43a8160ddd1811651b06649897
Chris Shaw <chris.shaw@quisitive.com>
2024-11-14 16:22:20 -0600

    Reworked the ExperienceComponent. Added rest of experience.

---
+++ src/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace Resume.Models;
+
+public class Company
+{
+    public string ImageLink { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
+    public string ExternalLink { get; set; } = string.Empty;
+    public string AltText => $"Logo for {CompanyName}";
+}
---
+++ src/Models/Experience.cs
@@ -0,0 +1,9 @@
+﻿namespace Resume.Models;
+
+public class Experience
+{
+    public string Title { get; set; } = string.Empty;
+    public string Date { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
+
+}
