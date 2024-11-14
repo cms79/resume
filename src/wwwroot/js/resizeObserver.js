1327736855f6fea9bb09dc94d8817f46e5370db4
Chris Shaw <chris.shaw@quisitive.com>
2024-11-14 17:22:58 -0600

    Add conditional wrapping using JSRuntime service

---
+++ src/wwwroot/js/resizeObserver.js
@@ -0,0 +1,12 @@
+﻿window.resizeObserver = {
+    observe: function (dotNetObject) {
+        const resizeObserver = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                const width = entry.contentRect.width;
+                dotNetObject.invokeMethodAsync('OnResize', width);
+            }
+        });
+
+        resizeObserver.observe(document.body);
+    }
+};
